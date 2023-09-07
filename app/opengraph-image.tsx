24ffd24a407d0604b951afc5668ed2e2e629e588
Samir-kumal <kumalsameer124@gmail.com>
2023-09-07 16:28:18 +0545

    changes

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import { ImageResponse } from "next/server";
-import photo from "../assets/img/layout.png"
+// import photo from "../assets/img/layout.png"
 export const size = {
     width: 900,
     height : 450,
@@ -12,7 +11,7 @@ export default async function og(){
 
     return new ImageResponse (
         <div tw="relative flex items-center justify-center">
-          <Image src={photo} alt="profile photo" />
+          <img src="../assets/img/layout.png" alt="profile photo" />
           <div tw="absolute flex bg-black opacity-50 inset-0 " />
           <div tw="absolute flex items-center top-2 w-full ">
             <p tw="text-white text-4xl flex font-bold m-5">Portfolio Site</p>
