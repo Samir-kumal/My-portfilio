5db3a0bdc424b0802a41bcba371bebc8defad32c
Samir Kumal <83536525+Samir-kumal@users.noreply.github.com>
2024-08-18 19:03:40 +0545

    chore: updated content

--- containers/Home.jsx
+++ containers/Home.jsx
@@ -1,29 +1,41 @@
-import Layout from '@/assets/svg/Layout'
-import Image from 'next/image'
-import React from 'react'
-import profile from "../assets/img/profile.png"
+import Layout from "@/assets/svg/Layout";
+import Image from "next/image";
+import React from "react";
+import profile from "../assets/img/profile.png";
 
 const Home = () => {
   return (
-   
-         <section className="home bd-grid" id="home">
-                <div className="home__data">
-                    <h1 className="home__title">Hi,<br/>I'am <span className="home__title-color">Samir</span> <br /> React Developer</h1>
+    <section className="home bd-grid" id="home">
+      <div className="home__data">
+        <h1 className="home__title">
+          Hi,
+          <br />
+          I'am <span className="home__title-color">Samir</span> <br /> React /
+          React Native Developer
+        </h1>
 
-                    <a href="#" className="button">Contact</a>
-                </div>
+        <a href="#" className="button">
+          Contact
+        </a>
+      </div>
 
-                <div className="home__social">
-                    <a href="" className="home__social-icon"><i className='bx bxl-linkedin'></i></a>
-                    <a href="" className="home__social-icon"><i className='bx bxl-behance' ></i></a>
-                    <a href="" className="home__social-icon"><i className='bx bxl-github' ></i></a>
-                </div>
-    
-                <div className="home__img relative">
-                  <Layout/>
-                </div>
-            </section>
-  )
-}
+      <div className="home__social">
+        <a href="" className="home__social-icon">
+          <i className="bx bxl-linkedin"></i>
+        </a>
+        <a href="" className="home__social-icon">
+          <i className="bx bxl-behance"></i>
+        </a>
+        <a href="" className="home__social-icon">
+          <i className="bx bxl-github"></i>
+        </a>
+      </div>
 
-export default Home
+      <div className="home__img relative">
+        <Layout />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
